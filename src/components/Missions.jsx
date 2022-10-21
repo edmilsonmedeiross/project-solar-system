bef23606bab29c20928192f6d58ff770c225f428
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-10-21 17:53:31 -0300

    requisito 9

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import Missiones from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {Missiones.map((mission) => (
+          <MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />
+        ))}
       </div>
     );
   }
